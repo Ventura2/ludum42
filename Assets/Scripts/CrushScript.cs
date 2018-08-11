15af4eb3d4a8631ae3175459e39bcf91149b5a2e
Jose manuel Garcia Maestre <josematrom@hotmail.com>
2018-08-11 13:56:20 +0200

    Wall colliding and crushing

---
+++ Assets/Scripts/CrushScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CrushScript : MonoBehaviour {
+
+
+    private BoxCollider2D boxCollider;
+
+    private void Start() {
+        boxCollider = this.GetComponent<BoxCollider2D>();
+    }
+
+    private void Update() {
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Platform")) {
+            Debug.Log("aplastado");
+        }
+    }
+}
+
