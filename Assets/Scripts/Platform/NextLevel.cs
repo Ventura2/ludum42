2f8b7836c3ed8058090b0404c1bdc60398e6ede6
Jose manuel Garcia Maestre <josematrom@hotmail.com>
2018-08-12 20:52:50 +0200

    First level, Credits

--- Assets/Scripts/Platform/NextLevel.cs
+++ Assets/Scripts/Platform/NextLevel.cs
@@ -7,7 +7,11 @@ public class NextLevel : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            GoNextLevel();
         }
+    }
+
+    public void GoNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
