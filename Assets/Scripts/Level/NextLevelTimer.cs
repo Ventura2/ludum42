0374c0e6e4a57b96a28c947ad9536a8862315ef0
Jose manuel Garcia Maestre <josematrom@hotmail.com>
2018-08-12 21:53:26 +0200

    Music improved

---
+++ Assets/Scripts/Level/NextLevelTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTimer : MonoBehaviour {
+
+    public int timeToNext = 10;
+
+    private float nextTime;
+	// Use this for initialization
+	void Start () {
+        nextTime = Time.time + timeToNext;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Time.time > nextTime || Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1")) {
+            NextLevel();
+        }
+
+	}
+    
+    private void NextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
