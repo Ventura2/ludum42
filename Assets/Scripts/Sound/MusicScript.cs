50e27c6a89ac77fe02e402f7f9bbe684ac84ad5b
Jose manuel Garcia Maestre <josematrom@hotmail.com>
2018-08-12 18:19:41 +0200

    Music, fish powerup added

---
+++ Assets/Scripts/Sound/MusicScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+
+    public static MusicScript instance;
+
+
+    void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
