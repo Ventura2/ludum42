ab46e392d15dc81131de4c04742514f067fb1b66
Jose Manuel Garcia Maestre <jose.mgmaestre@ricston.com>
2018-09-02 13:08:23 +0200

    Hotfix: Raycast disabled for joystick and small joystick, raycast enabled only for clickable zones

---
+++ Assets/Scripts/Learnning/TestClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestClick : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+
+    public string message;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+        Debug.Log("Pointer down: " + eventData.pointerId + " message: " + message);
+    }
+}
